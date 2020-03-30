a0b73a4d533c7be3e8ae52649bbaaaf2f5b3e398
cmaughan <mornymorny@gmail.com>
2020-03-30 16:10:05 +0100

    Linux fixes@

--- config_all.sh
+++ config_all.sh
@@ -2,5 +2,9 @@
 
 mkdir build
 cd build
-cmake -G "Unix Makefiles" ../
+if [ "$1" != "" ] ; then
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$1 ../
+else
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../
+fi
 cd ../
