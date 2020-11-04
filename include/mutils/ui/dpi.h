9fd579a18fdb1eb57085d5ff710705994e5be04e
Chris Maughan <cmaughan@snarlsoftware.com>
2020-11-04 09:45:17 +0000

    Add new dpi macro

--- include/mutils/ui/dpi.h
+++ include/mutils/ui/dpi.h
@@ -16,4 +16,9 @@ void check_dpi();
 void set_dpi(const NVec2f& val);
 float dpi_pixel_height_from_point_size(float pointSize, float pixelScaleY);
 
+#define MDPI_VEC2(value) (value * dpi.scaleFactorXY)
+#define MDPI_Y(value) (value * dpi.scaleFactorXY.y)
+#define MDPI_X(value) (value * dpi.scaleFactorXY.x)
+#define MDPI_RECT(value) (value * dpi.scaleFactorXY)
+
 } // MUtils
