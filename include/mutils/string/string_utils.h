dbabdd6b9c4813d651d6ad311d923f9a0057031c
Chris Maughan <mornymorny@gmail.com>
2021-02-23 09:19:53 +0000

    Improve timer utils

--- include/mutils/string/string_utils.h
+++ include/mutils/string/string_utils.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <iomanip>
 
 namespace MUtils
 {
--- src/time/timer.cpp
+++ src/time/timer.cpp
@@ -1,42 +1,20 @@
-#include <chrono> // Timing
-#include <iomanip>
+#include <chrono>
+#include <sstream>
+#include <string>
+#include <date/date.h>
 
 #include "mutils/logger/logger.h"
 #include "mutils/time/timer.h"
 #include <mutils/time/profiler.h>
 
 using namespace std::chrono;
+using namespace date;
+using namespace MUtils;
 
 namespace MUtils
 {
 
 timer globalTimer;
-
-uint64_t timer_get_time_now()
-{
-    return duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count();
-}
-
-void timer_start(timer& timer)
-{
-    timer_restart(timer);
-}
-
-void timer_restart(timer& timer)
-{
-    timer.startTime = duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count();
-}
-
-uint64_t timer_get_elapsed(const timer& timer)
-{
-    auto now = duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count();
-    return now - timer.startTime;
-}
-
-double timer_get_elapsed_seconds(const timer& timer)
-{
-    return timer_to_seconds(timer_get_elapsed(timer));
-}
 
 double timer_to_seconds(uint64_t value)
 {
@@ -46,6 +24,98 @@ double timer_to_seconds(uint64_t value)
 double timer_to_ms(uint64_t value)
 {
     return double(value / 1000000.0);
+}
+
+date::sys_time<std::chrono::milliseconds> sys_time_from_iso_8601(const std::string& str)
+{
+    std::istringstream in{ str };
+    date::sys_time<std::chrono::milliseconds> tp;
+    in >> date::parse("%FT%TZ", tp);
+    if (in.fail())
+    {
+        in.clear();
+        in.exceptions(std::ios::failbit);
+        in.str(str);
+        in >> date::parse("%FT%T%Ez", tp);
+    }
+    return tp;
+}
+
+DateTime datetime_from_iso_8601(const std::string& str)
+{
+    auto t = sys_time_from_iso_8601(str);
+    return DateTime(duration_cast<seconds>(t.time_since_epoch()));
+}
+/*
+std::string datetime_to_iso_8601_string(DateTime dt)
+{
+    return date::format("%y_%m_%d_%H_%M_%S", dt);
+}
+*/
+
+std::string datetime_to_iso_8601(DateTime tp)
+{
+    return date::format("%FT%TZ", date::floor<seconds>(tp));
+}
+
+DateTime datetime_now()
+{
+    return DateTime(date::floor<std::chrono::seconds>(std::chrono::system_clock::now()));
+}
+
+DateTime datetime_from_seconds(uint64_t t)
+{
+    return DateTime(std::chrono::seconds(t));
+}
+
+DateTime datetime_from_seconds(std::chrono::seconds s)
+{
+    return DateTime(s);
+}
+
+DateTime datetime_from_timer_start(timer& timer)
+{
+    return DateTime(seconds(timer_to_epoch_utc_seconds(timer)));
+}
+
+// Convert DateTime to string 
+std::string datetime_to_string(DateTime d, DateTimeFormat format)
+{
+    auto dp = date::floor<date::days>(d);
+    auto ymd = date::year_month_day{ dp };
+    auto tm = make_time(d - dp);
+    std::ostringstream str;
+    switch (format)
+    {
+    default:
+    case DateTimeFormat::JsonDayMonthYear:
+        str << ymd.day() << "/" << ymd.month() << "/" << ymd.year();
+        return str.str();
+    case DateTimeFormat::YearMonthDay:
+        str << ymd.day() << " " << ymd.month() << " " << ymd.year();
+        return str.str();
+    case DateTimeFormat::YearMonthDayTime:
+        str << ymd.day() << " " << ymd.month() << " " << ymd.year() << " - " << tm;
+        return str.str();
+    case DateTimeFormat::DayMonth:
+        str << ymd.day() << " " << ymd.month();
+        return str.str();
+    case DateTimeFormat::Month:
+        str << ymd.month();
+        return str.str();
+    case DateTimeFormat::Year:
+        str << ymd.year();
+        return str.str();
+    case DateTimeFormat::Day:
+        str << ymd.day();
+        return str.str();
+    case DateTimeFormat::Time:
+        str << tm;
+        return str.str();
+    case DateTimeFormat::Value:
+        str << d.time_since_epoch().count();
+        return str.str();
+    }
 }
 
 } // namespace MUtils
